538fec97b20d81375138965128df7b03aad34750
chandailrc <1chandailrc1@gmail.com>
2016-05-21 16:06:04 -0400

    Add files via upload

--- dvm_GPSINS_meas.m
+++ dvm_GPSINS_meas.m
@@ -40,20 +40,25 @@ replace = 0;
 % >>>>>>>>START: EKF PARAMETERS<<<<<<<<<<<<<
     NN = last_frame - first_frame + 1;
     n_states=13;
-    n_meas_states = 9;                          % x, y, theta
+    n_meas_states = 9;
+%     n_meas_states_dash = 3;
     q_proc=0.1;                                 %std of process 
-    r_meas=0.1;                                 %std of measurement
+    r_meas= 0.1;                                 %std of measurement
     
     Q_proc=q_proc^2*eye(n_states);              % covariance of process
-    [Q_proc_E, Q_proc_e] = eig(Q_proc);
-    R_meas=r_meas^2*eye(n_meas_states);         % covariance of measurement  
+    Q_proc_NoiseM = Q_proc;
+    Q_proc_NoiseM(5,5) = 0.001^2;
+    [Q_proc_E, Q_proc_e] = eig(Q_proc_NoiseM);
+    R_meas=r_meas^2*eye(n_meas_states);         % covariance of measurement
+%     R_meas_dash=r_meas^2*eye(n_meas_states_dash);
     [R_meas_E, R_meas_e] = eig(R_meas);
+%     [R_meas_E_dash, R_meas_e_dash] = eig(R_meas_dash);
     dt = 0.1;
     delta = 0;
     WB = 2.71;
         % >>>>>>>>>>>>>NOISE START<<<<<<<<<<<<<<<<<<<<<
         mu = zeros(3,1);
-        QR = [r_meas^2 0 0; 0 r_meas^2 0; 0 0 (r_meas*0.1)^2]; % Covariance
+        QR = [0.1^2 0 0; 0 0.1^2 0; 0 0 (0.01*0.01)^2]; % Covariance
         n = length(QR(:,1)); % Size
         [QRE, QRe] = eig(QR); % Eigenvectors and eigenvalues of Sigma
 
@@ -71,6 +76,13 @@ replace = 0;
        gtEulAng(:,i) = rotm2eul([X03(i,1:3); X03(i,5:7); X03(i,9:11)]);
     end
     gt = [gtdata(4,1:NN);gtdata(12,1:NN); gtEulAng(2,1:NN); gtEulAng(3,1:NN); gtEulAng(1,1:NN); gtdata(8,1:NN)];
+    
+    velX(1) = 0;
+    velY(1) = 0;
+    for i=2:NN
+        velX(i) = (gt(1,i)-gt(1,i-1))/dt;
+        velY(i)  = (gt(2,i)-gt(2,i-1))/dt;
+    end 
 %     gt = [x z yaw roll pitch y]
     % Trans roll == pitch
     % Trans pitch == yaw
@@ -84,10 +96,12 @@ replace = 0;
 
     s_state=[gt(1,1) ;0;gt(1,2);0;gt(1,3);0;gt(1,4);0;gt(1,5);0;gt(1,6);0;0];         % initial state
     x=s_state+q_proc*randn(n_states,1);               % initial state with noise
+%     x_dash = x;
     P = 0.1*eye(n_states);                            % initial state covraiance
+%     P_dash = P;
     x_store = zeros(n_states,NN);                     %estmate    
     meas_store = zeros(n_meas_states,NN);             %actual
-  
+%     meas_store_dash = zeros(n_meas_states_dash,NN);
   
 % >>>>>>>>>>>>>END: EKF PARAMTERS<<<<<<<<<<<<<<
 
@@ -147,18 +161,25 @@ for frame=first_frame:last_frame
       
 %       meas = [Tr_total{k}(1,4); Tr_total{k}(3,4);eulAng(2,k)];%; gt_meas(1,k);gt_meas(2,k);gt_meas(3,k)];
       meas = [Tr_ekf_total{k}(1,4); Tr_ekf_total{k}(3,4);eulAngEkf(2,k);eulAngEkf(3,k);eulAngEkf(1,k);Tr_ekf_total{k}(2,4); gt_meas(1,k);gt_meas(2,k);gt_meas(3,k)];
-      meas_store(:,k)= meas;                            % save actual state
+      meas_store(:,k)= meas;                            % save actual state 
+      
+%       meas_dash = [gt_meas(1,k);gt_meas(2,k);gt_meas(3,k)];
+%       meas_store_dash(:,k)= meas_dash; 
       
       e = Q_proc_E*sqrt(Q_proc_e)*randn(n_states,1);
       d = R_meas_E*sqrt(R_meas_e)*randn(n_meas_states,1);
       
       
       % x_handle = [x vx z vz yaw yawR roll rollR pitch pitchR y vy V]
-      % x_handle=@(x)[x(1)+x(2)*dt;x(13)*cos(x(5));x(3)+x(4)*dt;x(13)*sin(x(5));x(5)+x(6)*dt;x(6); x(7)+x(8)*dt; x(8); x(9)+x(10)*dt; x(10);x(11)+x(12)*dt;x(12); x(13)];
-      x_handle=@(x)[x(1)+(x(2)*dt)+e(1);x(2)+e(2);x(3)+(x(4)*dt)+e(3);x(4)+e(4);x(5)+(x(6)*dt)+e(5);x(6)+e(6); x(7)+x(8)*dt+e(7); x(8)+e(8); x(9)+(x(10)*dt)+e(9); x(10)+e(10);x(11)+(x(12)*dt)+e(11);x(12)+e(12); x(13)+e(13)];
-    
-      % y_handle=@(x)[x(2)/dt;x(4)/dt;x(6)/dt];      % measurement equation
+      x_handle=@(x)[x(1)+(x(2)*dt)+e(1);(x(13)*sin(x(5)))+e(2);x(3)+(x(4)*dt)+e(3);(x(13)*cos(x(5)))+e(4);x(5)+(x(6)*dt)+e(5);x(6)+e(6); x(7)+(x(8)*dt)+e(7); x(8)+e(8); x(9)+(x(10)*dt)+e(9); x(10)+e(10);x(11)+(x(12)*dt)+e(11);x(12)+e(12); x(13)+e(13)];
+%       x_handle=@(x)[x(1)+(x(2)*dt)+e(1);x(2)+e(2);x(3)+(x(4)*dt)+e(3);x(4)+e(4);x(5)+(x(6)*dt)+e(5);x(6)+e(6); x(7)+x(8)*dt+e(7); x(8)+e(8); x(9)+(x(10)*dt)+e(9); x(10)+e(10);x(11)+(x(12)*dt)+e(11);x(12)+e(12); x(13)+e(13)];
+%       x_handle_dash=@(x)[x(1)+(x(2)*dt)+e(1);x(2)+e(2);x(3)+(x(4)*dt)+e(3);x(4)+e(4);x(5)+(x(6)*dt)+e(5);x(6)+e(6); x(7)+x(8)*dt+e(7); x(8)+e(8); x(9)+(x(10)*dt)+e(9); x(10)+e(10);x(11)+(x(12)*dt)+e(11);x(12)+e(12); x(13)+e(13)];
+      
+
+%       y_handle=@(x)[x(2)/dt;x(4)/dt;x(6)/dt];      % measurement equation
       y_handle=@(x)[x(1)+d(1);x(3)+d(2);x(5)+d(3);x(7)+d(4);x(9)+d(5);x(11)+d(6);x(1)+d(7);x(3)+d(8);x(5)+d(9)];
+      
+%       y_handle_dash=@(x)[x(1)+d(7);x(3)+d(8);x(5)+d(9)];
 %       x
 %       meas
 %       P
@@ -166,12 +187,14 @@ for frame=first_frame:last_frame
 %       Q_proc
 %       R_meas
       [x, P] = ekf(x_handle,x,P,y_handle,meas,Q_proc,R_meas);
+%       [x_dash, P_dash] = ekf(x_handle_dash,x_dash,P_dash,y_handle_dash,meas_dash,Q_proc,R_meas_dash);
       Rekf = eul2rotm([x(9) x(5) x(7)]);
 %       disp([x(9) x(5) x(7)]);
       Tekf = [x(1);x(11);x(3)];
       Tr_ekf_total{k} = [Rekf Tekf; 0 0 0 1];
 %       disp(rotm2eul(Tr_ekf_total{k}(1:3,1:3)));
       x_store(:,k-1) = x;                            % save estimate
+%       x_store_dash(:,k-1) = x_dash; 
         
   end
 
@@ -187,39 +210,105 @@ for frame=first_frame:last_frame
 end
 
 
-figure;
-  for k=1:3                                % plot results
-    subplot(3,1,k)
-    hold on;
-%     plot(1:NN, meas_store(k,:), '-', 1:NN, x_store(k,:), '--')
-%     plot(1:NN, gt(k,:), 'b-')
-      plot(1:NN, gt(k,:), 'b-',1:NN, x_store(2*k-1,:), 'r--');
-%     plot(1:NN, x_store(k,:), '--')
-  end
+% figure;
+%   for k=1:3                                % plot results
+%     subplot(3,1,k)
+%     hold on;
+% %     plot(1:NN, meas_store(k,:), '-', 1:NN, x_store(k,:), '--')
+% %     plot(1:NN, gt(k,:), 'b-')
+%       plot(1:NN, gt(k,:), 'b-',1:NN, x_store(2*k-1,:), 'r--', 1:NN, gt_meas(k,:), 'g--');
+% %     plot(1:NN, x_store(k,:), '--')
+%   end
  
-  figure;
-  for k=2:NN
-  plot([Tr_total{k-1}(1,4) Tr_total{k}(1,4)], ...
-         [Tr_total{k-1}(3,4) Tr_total{k}(3,4)],'-xb','LineWidth',1)
-     hold on;
-  end
-  plot(gt(1,1:NN), gt(2,1:NN), 'r--', 'LineWidth',2);
-  axis equal;
-  hold on;
-  for k=2:NN-1
-      plot([x_store(1,k-1) x_store(1,k)], ...
-           [x_store(3,k-1) x_store(3,k)],'-g+','LineWidth',1)
-      hold on;
-      theta = x_store(5,k);
-      r = 1; % magnitude (length) of arrow to plot
-      x = x_store(1,k); y = x_store(3,k);
-      u = r * cos(theta); % convert polar (theta,r) to cartesian
-      v = r * sin(theta);
-      quiver(x,y,u,v);
-      hold on;
-  end
-  hold off;
+%   figure;
+%   for k=2:NN
+%   plot([Tr_total{k-1}(1,4) Tr_total{k}(1,4)], ...
+%          [Tr_total{k-1}(3,4) Tr_total{k}(3,4)],'-xb','LineWidth',1)
+%      hold on;
+%   end
+%   plot(gt(1,1:NN), gt(2,1:NN), 'r--', 'LineWidth',2);
+%   axis equal;
+%   hold on;
+
+% figure;
+%   for k=2:NN-1
+% %       plot([gt(1,k+1-1) gt(1,k+1)],...
+% %            [gt(2,k+1-1) gt(2,k+1)],'-ro','LineWidth',1)
+% %       hold on;
+%       plot([x_store(1,k-1) x_store(1,k)], ...
+%            [x_store(3,k-1) x_store(3,k)],'-g+','LineWidth',1)
+%       hold on;
+%       theta = x_store(5,k);
+%       r = 1; % magnitude (length) of arrow to plot
+%       x = x_store(1,k); y = x_store(3,k);
+%       u = r * cos(theta); % convert polar (theta,r) to cartesian
+%       v = r * sin(theta);
+%       quiver(x,y,u,v);
+%       hold on;
+%       plot([gt_meas(1,k+1-1) gt_meas(1,k+1)],...
+%            [gt_meas(2,k+1-1) gt_meas(2,k+1)],'-bx','LineWidth',1)
+%       hold on;
+%   end
+%   xlabel('X');
+%   ylabel('Y');
+%   hold off;
   
+  figure
+  for k=1:3                                % plot results
+    subplot(5,1,k)
+    plot(1:NN, gt(k,:), 'b-', 1:NN, x_store(2*k-1,:), 'r--')
+  end
+  subplot(5,1,4)
+  plot(1:NN, velX, 'b-', 1:NN, x_store(2,:),'r--');
+  subplot(5,1,5)
+  plot(1:NN, velY, 'b-', 1:NN, x_store(4,:),'r--');
+%   for k=2:NN-1
+%       estDevX(k-1) = abs((x_store(1,k-1)-gt(1,k+1-1))/gt(1,k+1-1));
+%       estDevY(k-1) = abs((x_store(3,k-1)-gt(2,k+1-1))/gt(2,k+1-1));
+%       estDevX_dash(k-1) = abs((x_store_dash(1,k-1)-gt(1,k+1-1))/gt(1,k+1-1));
+%       estDevY_dash(k-1) = abs((x_store_dash(3,k-1)-gt(2,k+1-1))/gt(2,k+1-1));
+%       measDevX(k-1) = abs((gt_meas(1,k+1-1)-gt(1,k+1-1))/gt(1,k+1-1));
+%       measDevY(k-1) = abs((gt_meas(2,k+1-1)-gt(2,k+1-1))/gt(2,k+1-1));
+%   end
+%   
+%   mEDX = mean(estDevX);
+%   mEDY = mean(estDevY);
+%   mEDX_dash = mean(estDevX_dash);
+%   mEDY_dash = mean(estDevY_dash);
+%   mMDX = mean(measDevX);
+%   mMDY = mean(measDevY);
+  
+  %{
+  figure;
+   for k=2:NN-1
+%       plot([gt(1,k+1-1) gt(1,k+1)],...
+%            [gt(2,k+1-1) gt(2,k+1)],'-ro','LineWidth',1)
+%       hold on;
+      plot([x_store_dash(1,k-1) x_store_dash(1,k)], ...
+           [x_store_dash(3,k-1) x_store_dash(3,k)],'-g+','LineWidth',1)
+      hold on;
+      
+      plot([x_store(1,k-1) x_store(1,k)], ...
+           [x_store(3,k-1) x_store(3,k)],'-r+','LineWidth',1)
+      hold on;
+%       theta = x_store(5,k);
+%       r = 1; % magnitude (length) of arrow to plot
+%       x = x_store(1,k); y = x_store(3,k);
+%       u = r * cos(theta); % convert polar (theta,r) to cartesian
+%       v = r * sin(theta);
+%       quiver(x,y,u,v);
+%       hold on;
+      plot([gt_meas(1,k+1-1) gt_meas(1,k+1)],...
+           [gt_meas(2,k+1-1) gt_meas(2,k+1)],'-bx','LineWidth',1)
+      hold on;
+  end
+  xlabel('X');
+ylabel('Y');
+  hold off;
+%}
+%   figure;
+%   plot(1:NN, gt_meas(3,:),'b-',1:NN, x_store(5,:), 'r--');
+      
 %   figure;
 %   for k=1:3                                % plot results
 %     subplot(3,1,k)
@@ -241,13 +330,13 @@ figure;
 % plot(X03(1:200,4),X03(1:200,12), 'b')
 % axis equal
 
-figure;
-  for k=1:3                                % plot results
-    subplot(3,1,k)
-    hold on;
-%     plot(1:NN, meas_store(k,:), '-', 1:NN, x_store(k,:), '--')
-%     plot(1:NN, gt(k,:), 'b-')
-      plot(1:NN, gt_m(k,:), 'b-',1:NN, gt_meas(k,:), 'r--');
-%     plot(1:NN, x_store(k,:), '--')
-  end
+% figure;
+%   for k=1:3                                % plot results
+%     subplot(3,1,k)
+%     hold on;
+% %     plot(1:NN, meas_store(k,:), '-', 1:NN, x_store(k,:), '--')
+% %     plot(1:NN, gt(k,:), 'b-')
+%       plot(1:NN, gt_m(k,:), 'b-',1:NN, gt_meas(k,:), 'r--');
+% %     plot(1:NN, x_store(k,:), '--')
+%   end
 visualOdometryMonoMex('close');
